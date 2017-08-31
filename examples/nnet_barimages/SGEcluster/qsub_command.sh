f38f5047bc9f7acef67ce266c3c6f6ef91ff758d
Paul Rozdeba <prozdeba@physics.ucsd.edu>
2017-08-30 18:50:33 -0700

    Fixed qsub script so directories only created if they don't exist yet.

--- examples/nnet_barimages/SGEcluster/qsub_command.sh
+++ examples/nnet_barimages/SGEcluster/qsub_command.sh
@@ -1,8 +1,8 @@
-mkdir 1ex
-mkdir 2ex
-mkdir 10ex
-mkdir 100ex
-mkdir output
-mkdir error
+mkdir -p 1ex
+mkdir -p 2ex
+mkdir -p 10ex
+mkdir -p 100ex
+mkdir -p output
+mkdir -p error
 
 qsub -t 1-400 -tc 100 -N barmultiM submit_multiM.sh
